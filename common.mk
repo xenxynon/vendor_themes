5ff51f4ae184e8038b4e78bcdc3daf68c0ec7c03
xenxynon <flynryder427@gmail.com>
2025-02-06 18:14:46 +0530

    fonts: refresh overlays  - move everything to a common makefile  - remove non existent product packages  - font overlays made using https://github.com/ProtonAOSP/android_vendor_proton/blob/sc-qpr3/fonts/generate_overlays.py
    Change-Id: I28ad9d45aab27e10a0e0968ea5b55223732fd622
    Signed-off-by: xenxynon <flynryder427@gmail.com>

--- common.mk
+++ common.mk
@@ -1,5 +1,37 @@
 # Fonts
-include vendor/themes/fonts.mk
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,vendor/themes/fonts//prebuilt,$(TARGET_COPY_OUT_PRODUCT)/fonts) \
+    vendor/themes/fonts/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml
 
-# Icon Shapes
-include vendor/themes/iconshapes.mk
+# Font overlays
+PRODUCT_PACKAGES += \
+    FontArvoOverlay \
+    FontBarlowOverlay \
+    FontBigShouldersTextOverlay \
+    FontFrauncesOverlay \
+    FontKarlaOverlay \
+    FontLatoOverlay \
+    FontLustriaOverlay \
+    FontRobotoOverlay \
+    FontRubikOverlay \
+    FontUbuntuOverlay \
+    FontZillaSlabOverlay \
+    FontHarmonyOSSansOverlay \
+    FontInterOverlay \
+    FontLinotteOverlay \
+    FontManropeOverlay \
+    FontOnePlusSansOverlay \
+    FontSlateFromOPOverlay \
+    FontUbuntuMonoOverlay
+
+# Icon shape overlays
+PRODUCT_PACKAGES += \
+    IconShapeRoundedRectOverlay \
+    IconShapeSquareOverlay \
+    IconShapeSquircleOverlay \
+    IconShapeTeardropOverlay \
+    IconShapeCylinderOverlay \
+    IconShapeHexagonOverlay \
+    IconShapePebbleOverlay \
+    IconShapeTaperedRectOverlay \
+    IconShapeVesselOverlay
