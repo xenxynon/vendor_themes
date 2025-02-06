04dc96542e9ba7820c7cba567abf0818dffa9502
xenxynon <flynryder427@gmail.com>
2025-02-06 16:04:45 +0000

    common: unnecessary /
    Change-Id: I0cb51486c22fcff6505eab23abfdf733cc6fe1a4

--- common.mk
+++ common.mk
@@ -1,6 +1,6 @@
 # Fonts
 PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files,*,vendor/themes/fonts//prebuilt,$(TARGET_COPY_OUT_PRODUCT)/fonts) \
+    $(call find-copy-subdir-files,*,vendor/themes/fonts/prebuilt,$(TARGET_COPY_OUT_PRODUCT)/fonts) \
     vendor/themes/fonts/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml
 
 # Font overlays
